57aec8f7294f80ae42f036ead633082765d2f7d0
eranchetz <eran@chetz.org>
2018-12-24 14:59:09 +0200

    add wait from query params and switch to  middleware

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # whs
 Wait HTTP Server | An HTTP Server that holds the request hostage for n seconds
 
+#### defaults
+* wait = 0s
+* ignore-sigterm = true
 
 ## Run 
 
 ```go
 docker run -ti --rm  -p 8080:8080 eranchetz/whs -wait 2 -ignore-sigterm false
 ```
+
+# Use 
+
+```bash
+curl localhost:8080/?wait=750ms
+```
+
+wait is parsed by these [rules](https://golang.org/pkg/time/#ParseDuration)
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,15 +17,65 @@ import (
 var flagwait int
 var flagIgnoresSIGTERM bool
 
+type request struct {
+	URL     string      `json:"url"`
+	Method  string      `json:"method"`
+	Headers http.Header `json:"headers"`
+	Body    []byte      `json:"body"`
+}
+
 func init() {
-	flag.IntVar(&flagwait, "wait", 5, "wait time in seconds for http req")
+	flag.IntVar(&flagwait, "wait", 0, "wait time in seconds for http req")
 	flag.BoolVar(&flagIgnoresSIGTERM, "ignore-sigterm", true, "ignore SIGTERM [default=true]")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("got request processing for", flagwait, "sec", r.RemoteAddr, r.URL)
-	time.Sleep(time.Duration(flagwait) * time.Second)
-	fmt.Fprintf(w, "Ok, Done after 5 sec")
+// middlewareWait makes the response wait, default is 0 you can set it by requesting:
+// GET /?wait=5s
+func middlewareWait(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		duration := time.Duration(flagwait) * time.Second
+		var err error
+
+		u := r.URL
+		qp, _ := url.ParseQuery(u.RawQuery)
+		if qp["wait"] != nil {
+			duration, err = time.ParseDuration(qp["wait"][0])
+			if err != nil {
+				fmt.Fprintf(w, "error parsing wait %v", err)
+			}
+		}
+		log.Println("got request processing for ", duration, r.RemoteAddr, r.URL)
+		time.Sleep(duration)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// handlerHealth returns ok
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
+}
+
+// handlerEcho returns the request headers and body in json form
+func handlerEcho(rw http.ResponseWriter, r *http.Request) {
+	var err error
+	rr := &request{}
+	rr.Method = r.Method
+	rr.Headers = r.Header
+	rr.URL = r.URL.String()
+	rr.Body, err = ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rrb, err := json.Marshal(rr)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(rrb)
 }
 
 func catchSignal(sigc chan os.Signal) {
@@ -44,7 +97,11 @@ func main() {
 		go catchSignal(sigc)
 	}
 	// HTTP Server
-	http.HandleFunc("/", handler)
+	handleEcho := http.HandlerFunc(handlerEcho)
+	handleHealth := http.HandlerFunc(handlerHealth)
+	http.Handle("/", middlewareWait(handleEcho))
+	http.Handle("/health", middlewareWait(handleHealth))
+
 	log.Printf("WaitHTTPServer started on :8080 with %dsec wait , PID=%d, IgnoresSIGTERM=%t", flagwait, os.Getpid(), flagIgnoresSIGTERM)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
